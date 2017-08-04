02355b01496e3b938ffeb2695b74df002037f6c7
Carlos Vasquez <hispanasian@gmail.com>
2017-08-04 00:25:21 -0400

    Updated git-status-color.sh

--- .git-status-color.sh
+++ .git-status-color.sh
@@ -3,29 +3,35 @@
 
 __git_status_color ()
 {
-	local color_red='\e[0;31m'
-	local color_yellow='\e[0;33m'
-	local color_green='\e[0;32m'
-	local color_ochre='\e[38;5;95m'
-	local color_blue='\e[0;34m'
-	local color_white='\e[0;37m'
-	local color_reset='\e[0m'
+	if [[ -n ${ZSH_VERSION-} ]]; then
+		local color_red='%F{red}'
+		local color_yellow='%F{yellow}'
+		local color_green='%F{green}'
+		local color_blue='%F{blue}'
+		local color_white='%F{white}'
+	else
+		local color_red='\e[31m'
+		local color_yellow='\e[93m'
+		local color_cyan='\e[36m'
+		local color_blue='\e[34m'
+		local color_white='\e[97m'
+	fi
 
 	local git_status="$(git status 2> /dev/null)"
 	local status_color="";
 
 	if [[ "$git_status" =~ "Untracked files" ]]; then
-		status_color="$color_red"
-	elif [[ "$git_status" =~ "Changes to be committed" ]]; then
-		status_color="$color_green"
+		status_color=$color_red
+	elif [[ "$git_status" =~ "Changes not staged for commit" ]]; then
+		status_color=$color_blue
 	elif [[ "$git_status" =~ "Your branch is ahead of" ]]; then
-		git_status="$color_yellow"
+		status_color=$color_green
 	elif [[ "$git_status" =~ "nothing to commit" ]]; then
-		git_status="$color_blue"
+		status_color=$color_white
 	else
-		git_status="$color_ochre"
+		status_color=$color_white
 	fi
 
-	echo -e "$status_color"
+	echo -e $status_color
 }
 
