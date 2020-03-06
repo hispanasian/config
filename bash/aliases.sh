ecb03c048d20579f38bb97983d1e936cadc890f6
Carlos Vasquez <hispanasian@gmail.com>
2020-03-06 11:27:23 -0500

    Updated aliases

--- bash/aliases.sh
+++ bash/aliases.sh
@@ -1,4 +1,4 @@
-# enable color support of ls and also add handy aliases
+# check if a command exists with hash <command> 2> /dev/null as per https://stackoverflow.com/questions/592620/how-can-i-check-if-a-program-exists-from-a-bash-script
 alias ls='ls --color=auto'
 alias dir='dir --color=auto'
 alias vdir='vdir --color=auto'
@@ -7,8 +7,6 @@ alias grep='grep --color=auto'
 alias fgrep='fgrep --color=auto'
 alias egrep='egrep --color=auto'
 
-alias powershell='pwsh'
-
 # fly
 alias fm='fly -t mia'
 alias fq='fly -t qrk'
@@ -16,12 +14,29 @@ alias fq='fly -t qrk'
 # vault
 alias vlcpi='export VAULT_ADDR=https://vault-d-dc.mia.ucloud.int:8200 && vault login -method cert -client-cert=$HOME/.ssh/vault/customs-pit.pem -client-key=$HOME/.ssh/vault/customs-pit-key.pem'
 
+if hash pwsh 2>/dev/null; then
+  alias powershell='pwsh'
+fi
+
 # wsl aliases
-if grep -q Microsoft /proc/version; then
-    # WSL
-    alias clip='clip.exe'
+if grep -q Microsoft /proc/version 2> /dev/null; then
+  # we're running in wsl
+  alias clip='clip.exe'
+  alias explorer='explorer.exe'
+
+  if hash docker.exe 2> /dev/null && ! hash docker 2> /dev/null; then
     alias docker='docker.exe'
-else
-    # Native Linux
-  echo "native Linux"
+  fi
+
+  if hash gitk.exe 2> /dev/null && ! hash gitk 2> /dev/null; then
+    alias gitk='gitk.exe'
+  fi
+
+  if hash fly.exe 2> /dev/null && ! hash fly 2> /dev/null; then
+    alias fly='fly.exe'
+  fi
+
+  if hash vault.exe 2> /dev/null && ! hash vault 2> /dev/null; then
+    alias vault='vault.exe'
+  fi
 fi
--- powershell/aliases.ps1
+++ powershell/aliases.ps1
@@ -1,1 +1,29 @@
 function vlcpi {  $env:VAULT_ADDR = "https://vault-d-dc.mia.ucloud.int:8200"; vault login -method cert -client-cert="$env:HOME\.ssh\vault\customs-pit.pem" -client-key="$env:HOME\.ssh\vault\customs-pit-key.pem" $args }
+
+# wsl aliases
+if(Select-String -Path /proc/version -Pattern "Microsoft" -ErrorAction SilentlyContinue)
+{
+    # we're running in wsl
+    Set-Alias -Name clip -Value clip.exe
+    Set-Alias -Name explorer -Value explorer.exe
+    
+    if ((Get-Command docker.exe -ErrorAction SilentlyContinue) -and -not (Get-Command docker -ErrorAction SilentlyContinue))
+    {
+        Set-Alias -Name docker -Value docker.exe
+    }
+
+    if ((Get-Command gitk.exe -ErrorAction SilentlyContinue) -and -not (Get-Command gitk -ErrorAction SilentlyContinue))
+    {
+        Set-Alias -Name gitk -Value gitk.exe
+    }
+
+    if ((Get-Command fly.exe -ErrorAction SilentlyContinue) -and -not (Get-Command fly -ErrorAction SilentlyContinue))
+    {
+        Set-Alias -Name fly -Value fly.exe
+    }
+
+    if ((Get-Command vault.exe -ErrorAction SilentlyContinue) -and -not (Get-Command vault -ErrorAction SilentlyContinue))
+    {
+        Set-Alias -Name vault -Value vault.exe
+    }
+}
