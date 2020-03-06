3bc5a7db2499b52b44c4057a88ac9e278cf213e7
Carlos Vasquez <hispanasian@gmail.com>
2020-03-06 08:50:52 -0500

    Added more WSL aliases

--- bash/aliases.sh
+++ bash/aliases.sh
@@ -17,6 +17,11 @@ alias fq='fly -t qrk'
 alias vlcpi='export VAULT_ADDR=https://vault-d-dc.mia.ucloud.int:8200 && vault login -method cert -client-cert=$HOME/.ssh/vault/customs-pit.pem -client-key=$HOME/.ssh/vault/customs-pit-key.pem'
 
 # wsl aliases
-if [ -f /mnt/c/WINDOWS/system32/clip.exe ]; then
+if grep -q Microsoft /proc/version; then
+    # WSL
     alias clip='clip.exe'
+    alias docker='docker.exe'
+else
+    # Native Linux
+  echo "native Linux"
 fi
